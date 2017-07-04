646e5c02e11c0d558b6c3c1f84d3845ec727a607
iduhao38426 <iduhao38426@163.com>
2017-07-04 14:48:28 +0800

    write script

--- arg.sh
+++ arg.sh
@@ -5,14 +5,40 @@ E_BADARGS=65
 if [ ! -n "$1" ]
 then
 	echo "Usage: `basename $0` argument1 argument2 etc."
-
 #exit $E_BADARGS
 
-#fi
-else 
-	echo "Usage: `basename $0` 's argument1 is $1."
+fi
+
+echo
+echo "++++++++++++++++++++++++++++++++++++++++"
+index=1
+
+echo "Listing args with \"\$*\":"
+for arg in "$*"
+do
+echo "Arg #$index = $arg"
+let "index+=1"
+done
+echo "Entire arg list seen as single word."
+
 echo
 
-#index=1
-
-#echo "Listing args with \"\$*\":"
+index=1
+echo "Listing args with \"\$@\":"
+for arg in "$@"
+do
+echo "Arg #$index = $arg"
+let "index+=1"
+done
+echo "Arg list seen as separate words."
+echo
+echo
+index=1
+echo "Listing args with \$* (unquoted):"
+for arg in $*
+do
+echo "Arg #index = $arg"
+let "index+=1"
+done
+echo "Arg list seen as separate words."
+exit 0
