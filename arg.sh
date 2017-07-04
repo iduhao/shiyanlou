5b242526b9b2c8ce7f8bf62c4149d79b3b35a923
iduhao38426 <iduhao38426@163.com>
2017-07-04 11:05:23 +0800

    break

--- arg.sh
+++ arg.sh
@@ -1,4 +1,18 @@
-#/bin/bash
+#!/bin/bash
 #
-#通过$*和$@列出所有参数 
+E_BADARGS=65
 
+if [ ! -n "$1" ]
+then
+	echo "Usage: `basename $0` argument1 argument2 etc."
+
+#exit $E_BADARGS
+
+#fi
+else 
+	echo "Usage: `basename $0` 's argument1 is $1."
+echo
+
+#index=1
+
+#echo "Listing args with \"\$*\":"
